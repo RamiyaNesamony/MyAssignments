3e39aa46acfee5b31418906119085561bd02a6c3
RamiyaNesamony <127000143+RamiyaNesamony@users.noreply.github.com>
2023-03-05 08:37:11 +0530

    Add files via upload

---
+++ day1/HelloSelenium.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ //type syso type (ctrl+spacebar)
+		System.out.println("I am going to do it");
+	}
+
+}
+ 
